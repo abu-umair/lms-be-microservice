8a834c24fcb08d909f7535cc42498e96cf771311
abu-umair <mrdwnw93@gmail.com>
2025-12-17 13:15:55 +0700

    ++ logic ngecek email ke DB

--- internal/repository/auth_repository.go
+++ internal/repository/auth_repository.go
@@ -3,21 +3,44 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/abu-umair/lms-be-microservice/internal/entity"
 )
 
 type IAuthRepository interface {
-	GetUserByEmail(ctx context.Context, email string) (entity.Users, error) //?ctx: mengakses DB nya. User: entitynya (table di DB, terhubung di entity), (User, error) adalah return data atau error
+	GetUserByEmail(ctx context.Context, email string) (*entity.Users, error) //?ctx: mengakses DB nya. User: entitynya (table di DB, terhubung di entity), (User, error) adalah return data atau error
 }
 
 type authRepository struct {
 	db *sql.DB //?Menyimpan koneksi database
 }
 
-func (ar *authRepository) GetUserByEmail(ctx context.Context, email string) (entity.Users, error) {
+func (ar *authRepository) GetUserByEmail(ctx context.Context, email string) (*entity.Users, error) {
+	row := ar.db.QueryRowContext(ctx, "SELECT id, email, password, full_name FROM users WHERE email = $1 AND is_deleted = false", email)
 
+	if row.Err() != nil {
+		return nil, row.Err()
+	}
+
+	var user entity.Users
+	err := row.Scan(
+		&user.Id,
+		&user.Email,
+		&user.Password,
+		&user.FullName,
+	)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
 }
+
 
 func NewAuthRepository(db *sql.DB) IAuthRepository {
 	return &authRepository{db: db}
--- internal/service/auth.service.go
+++ internal/service/auth.service.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 
+	"github.com/abu-umair/lms-be-microservice/internal/repository"
+	"github.com/abu-umair/lms-be-microservice/internal/utils"
 	"github.com/abu-umair/lms-be-microservice/pb/auth"
 )
 
@@ -10,20 +12,34 @@ type IAuthService interface {
 	Register(ctx context.Context, request *auth.RegisterRequest) (*auth.RegisterResponse, error)
 }
 
-type authService struct{}
+type authService struct {
+	authRepository repository.IAuthRepository
+}
 
 func (as *authService) Register(ctx context.Context, request *auth.RegisterRequest) (*auth.RegisterResponse, error) {
 	//? ngecek email ke DB
 	//* layer repository, utk akses DB (clean arsitektur)
+	user, err := as.authRepository.GetUserByEmail(ctx, request.Email)
+	if err != nil || user == nil {
+		return nil, err
+	}
+
+	if user != nil {
+		return &auth.RegisterResponse{
+			Base: utils.BadRequestResponse("User already exist"),
+		}, nil
+	}
 
 	//? jika emal sudah terdaftar/ada, di error in
 
 	//? Hash password
 
-	//? Insert ke DB 
+	//? Insert ke DB
 	return &auth.RegisterResponse{}, nil
 }
 
-func NewAuthService() IAuthService {
-	return &authService{}
+func NewAuthService(authRepository repository.IAuthRepository) IAuthService {
+	return &authService{
+		authRepository: authRepository,
+	}
 }
