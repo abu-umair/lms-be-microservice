7bb6820596bdafbcb8df98091f5c377d28df8b30
abu-umair <mrdwnw93@gmail.com>
2025-12-17 11:41:17 +0700

    ++ menghubungkan entity

---
+++ internal/entity/users.go
@@ -0,0 +1,36 @@
+package entity
+
+import "time"
+
+const (
+	UserRoleCustomer = "user"
+	UserRoleAdmin    = "admin"
+)
+
+type UserRoles struct {
+	Id        string
+	Name      string
+	Code      string
+	CreatedAt time.Time
+	CreatedBy *string
+	UpdatedAt time.Time
+	UpdatedBy *string
+	DeletedAt time.Time
+	DeletedBy *string
+	IsDeleted bool
+}
+
+type Users struct {
+	Id        string
+	FullName  string
+	Email     string
+	Password  string
+	RoleCode  string
+	CreatedAt time.Time
+	CreatedBy *string
+	UpdatedAt time.Time
+	UpdatedBy *string
+	DeletedAt time.Time
+	DeletedBy *string
+	IsDeleted bool
+}
--- internal/repository/auth_repository.go
+++ internal/repository/auth_repository.go
@@ -3,17 +3,19 @@ package repository
 import (
 	"context"
 	"database/sql"
+
+	"github.com/abu-umair/lms-be-microservice/internal/entity"
 )
 
 type IAuthRepository interface {
-	GetUserByEmail(ctx context.Context, email string) (User, error) //?ctx: mengakses DB nya. User: entitynya (table di DB, terhubung di entity), (User, error) adalah return data atau error
+	GetUserByEmail(ctx context.Context, email string) (entity.Users, error) //?ctx: mengakses DB nya. User: entitynya (table di DB, terhubung di entity), (User, error) adalah return data atau error
 }
 
 type authRepository struct {
 	db *sql.DB //?Menyimpan koneksi database
 }
 
-func (ar *authRepository) GetUserByEmail(ctx context.Context, email string) (User, error) {
+func (ar *authRepository) GetUserByEmail(ctx context.Context, email string) (entity.Users, error) {
 
 }
 
